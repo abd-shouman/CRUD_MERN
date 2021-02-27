7538346ccd0ec017ac5544630ae4d5f8eeaeaff9
abd-shouman <amsh-1992@hotmail.com>
2021-02-28 02:03:10 +0300

    converting usersList to dataGrid (update, delete not included)

--- client/src/components/User/Users.tsx
+++ client/src/components/User/Users.tsx
@@ -3,6 +3,8 @@ import { IUser } from '../../interfaces/types'
 import "./user.css"
 import UserItem from './UserItem'
 import { getAllUsers, updateUser, deleteUser } from './UserAPI'
+import { DataGrid, RowsProp, ColDef } from '@material-ui/data-grid';
+import { Grid } from '@material-ui/core'
 
 type UserProps = {
   users: Array<IUser>,
@@ -86,17 +88,47 @@ export default function Users(
 
   }, [])
 
+  const columns: ColDef[] = [
+    { field: 'firstName', headerName: 'First Name', width: 150 },
+    { field: 'lastName', headerName: 'Last Name', width: 150 },
+    { field: 'email', headerName: 'Email', width: 250 }
+  ];
+
+  // const rows: RowsProp = [
+  //   { id: 1, firstName: "Abd", lastName: "Shouman", email: "hello" },
+  //   { id: 2, firstName: "Abd", lastName: "Shouman", email: "hello" },
+  //   { id: 3, firstName: "Abd", lastName: "Shouman", email: "hello" },
+  //   { id: 4, firstName: "Abd", lastName: "Shouman", email: "hello" }
+  // ]
+
+  // const rows: RowsProp = [
+  //   { id: 1, col1: 'Hello', col2: 'World' },
+  //   { id: 2, col1: 'XGrid', col2: 'is Awesome' },
+  //   { id: 3, col1: 'Material-UI', col2: 'is Amazing' },
+  // ];
+
+  // const columns: ColDef[] = [
+  //   { field: 'col1', headerName: 'Column 1', width: 150 },
+  //   { field: 'col2', headerName: 'Column 2', width: 150 },
+  // ];
+
+
   return (
-    <table>
-      <tbody>
-        {users?.map((user, i) =>
-          <UserItem key={i} userindex={i} user={user}
-            // currentlyEditingIndex={currentlyEditingIndex} updateUserInfo={updateUserInfo} startEditing={startEditing} 
-            saveUser={saveUser}
-            removeUser={removeUser} />
-        )}
-      </tbody>
-    </table>
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid rows={users} columns={columns} getRowId={(row) => row._id} />
+    </div>
+    //   
+    // 
+    // <table>
+    //   <tbody>
+    //     {users?.map((user, i) =>
+    //       <UserItem key={i} userindex={i} user={user}
+    //         // currentlyEditingIndex={currentlyEditingIndex} updateUserInfo={updateUserInfo} startEditing={startEditing} 
+    //         saveUser={saveUser}
+    //         removeUser={removeUser} />
+    //     )}
+    //   </tbody>
+    // </table>
   );
 
 }
